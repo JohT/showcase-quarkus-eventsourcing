2647160b48361b667cc302f81dcae1e0832d482b
Johannes <johnnyt@gmx.at>
2019-09-22 20:33:15 +0200

    Temporary disabled Substrate tests

---
+++ showcase-quarkus-eventsourcing/src/test/java/io/github/joht/showcase/quarkuseventsourcing/service/account/AccountResourceSubstrateTest.java
@@ -0,0 +1,12 @@
+package io.github.joht.showcase.quarkuseventsourcing.service.account;
+
+import org.junit.jupiter.api.Disabled;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@Disabled
+@SubstrateTest
+public class AccountResourceSubstrateTest extends AccountResourceIntegrationTest {
+
+    // run the same tests
+}
---
+++ showcase-quarkus-eventsourcing/src/test/java/io/github/joht/showcase/quarkuseventsourcing/service/nickname/NicknamesProjectionManagementResourceSubstrateTest.java
@@ -0,0 +1,11 @@
+package io.github.joht.showcase.quarkuseventsourcing.service.nickname;
+
+import org.junit.jupiter.api.Disabled;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@Disabled
+@SubstrateTest
+public class NicknamesProjectionManagementResourceSubstrateTest extends NicknamesProjectionManagementResourceIntegrationTest {
+    // run the same tests
+}
